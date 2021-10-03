4c97012b12d26e6adf22eb80cd674192a385e50b
79216002-陈子庆 <60501687+ZC-tan@users.noreply.github.com>
2021-10-03 20:52:41 +0800

    judge update

--- Main.java
+++ Main.java
@@ -2,8 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String a = sc.nextLine();
-        System.out.println(a);
+
+        System.out.println("testing");
     }
 }
